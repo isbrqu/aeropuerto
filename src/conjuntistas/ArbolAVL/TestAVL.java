c1a951c10b28ecd59caa8bd07004ae0cf4b61a87
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-09-04 20:50:42 -0300

    organiza mejor el test

--- src/conjuntistas/ArbolAVL/TestAVL.java
+++ src/conjuntistas/ArbolAVL/TestAVL.java
@@ -7,15 +7,40 @@ public class TestAVL {
   }
 
   public static void main(String[] args) {
-    testSet1();
+    rotarIzquierda1();
+    rotarIzquierda2();
   }
 
-  public static void testSet1() {
-    print("TEST SET 1");
+  public static void rotarIzquierda1() {
+    print("TEST SET 1 - Rotacion izquierda");
     ArbolAVL arbol = new ArbolAVL();
-    int[] num = {1, 5, 9};
-    arbol.llenar(num);
+    int[] serie = {1, 5, 9};
+    arbol.llenar(serie);
     print(arbol);
   }
+
+  public static void rotarIzquierda2() {
+    print("TEST SET 2 - Rotacion izquierda");
+    ArbolAVL arbol = new ArbolAVL();
+    int[] serie = {3, 2, 5, 1, 8};
+    arbol.llenar(serie);
+    print(arbol);
+  }
+
+  // public static void rotarDerecha() {
+  //   print("TEST SET 2 - Rotacion derecha");
+  //   ArbolAVL arbol = new ArbolAVL();
+  //   int[] num = {9, 5, 1};
+  //   arbol.llenar(num);
+  //   print(arbol);
+  // }
+
+  // public static void rotarDerechaIzquierda() {
+  //   print("TEST SET n - Rotacion derecha-izquierda");
+  //   ArbolAVL arbol = new ArbolAVL();
+  //   int[] num = {1, 5, 9, 7, 9, 8};
+  //   arbol.llenar(num);
+  //   print(arbol);
+  // }
 
 }

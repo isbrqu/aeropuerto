c5b0d54a95fee1b67461532fd05f946a55c313c6
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-10-03 22:05:30 -0300

    simplifica los test de insertar

--- src/conjuntistas/ArbolAVL/TestAVL.java
+++ src/conjuntistas/ArbolAVL/TestAVL.java
@@ -1,19 +1,10 @@
 package conjuntistas.ArbolAVL;
-import conjuntistas.ArbolAVL.test.RotacionSimple;
-import conjuntistas.ArbolAVL.test.RotacionDoble;
 import conjuntistas.ArbolAVL.test.TestInsertar;
-import conjuntistas.ArbolAVL.test.TestEliminar;
-import conjuntistas.ArbolAVL.test.CrearArbol;
 
 public class TestAVL {
 
   public static void main(String[] args) throws Exception {
-    TestEliminar.RR();
-    TestEliminar.LR();
-    TestEliminar.LRR();
-    TestEliminar.RLR();
-    TestEliminar.LRR_RLR();
-    TestEliminar.RLR_LRR();
+    TestInsertar.callAll();
   }
 
 }

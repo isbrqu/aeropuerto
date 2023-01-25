dfdd06206eb75278af88e342abad68b82dae2f9c
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-25 09:17:25 -0300

    renombra NodoABB por Nodo

---
+++ src/conjuntistas/arbol/Nodo.java
@@ -0,0 +1,43 @@
+package conjuntistas.ArbolBB;
+
+public class Nodo {
+
+  private Comparable elemento;
+  private Nodo izquierdo;
+  private Nodo derecho;
+
+  public Nodo(Comparable elemento, Nodo izquierdo, Nodo derecho) {
+    this.elemento = elemento;
+    this.izquierdo = izquierdo;
+    this.derecho = derecho;
+  }
+
+  public Nodo(Comparable elemento) {
+    this(elemento, null, null);
+  }
+
+  public Comparable getElemento() {
+    return this.elemento;
+  }
+
+  public void setElemento(Comparable elemento) {
+    this.elemento = elemento;
+  }
+
+  public Nodo getIzquierdo() {
+    return this.izquierdo;
+  }
+
+  public void setIzquierdo(Nodo izquierdo) {
+    this.izquierdo = izquierdo;
+  }
+
+  public Nodo getDerecho() {
+    return this.derecho;
+  }
+
+  public void setDerecho(Nodo derecho) {
+    this.derecho = derecho;
+  }
+
+}

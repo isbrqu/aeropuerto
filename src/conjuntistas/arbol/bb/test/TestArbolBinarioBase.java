444a14045d9ec1e027b7a64dc5e7fe152ad562aa
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-26 10:03:38 -0300

    agrega un test básico

--- src/conjuntistas/arbol/bb/test/TestArbolBinarioBase.java
+++ src/conjuntistas/arbol/bb/test/TestArbolBinarioBase.java
@@ -1,11 +1,15 @@
-package conjuntistas.arbol.test;
+package conjuntistas.arbol.bb.test;
 
-import conjuntistas.arbol.ArbolBinarioBase;
+import conjuntistas.arbol.bb.ArbolBinarioBase;
+import conjuntistas.arbol.bb.ArbolBBBase;
+import conjuntistas.arbol.bb.ArbolBB;
 
 public class TestArbolBinarioBase {
 
   public static void main(String[] args) {
     ArbolBinarioBase a;
+    ArbolBBBase b;
+    ArbolBB c;
   }
 
 } 

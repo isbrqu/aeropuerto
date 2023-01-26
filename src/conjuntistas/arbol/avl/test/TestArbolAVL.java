bed967b9a269f60f99df37b2b4d7f41d949e0251
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-26 10:30:02 -0300

    agrega un test

---
+++ src/conjuntistas/arbol/avl/test/TestArbolAVL.java
@@ -0,0 +1,11 @@
+package conjuntistas.arbol.avl.test;
+
+import conjuntistas.arbol.avl.ArbolAVL;
+
+public class TestArbolAVL {
+
+  public static void main(String[] args) {
+    ArbolAVL a;
+  }
+
+} 

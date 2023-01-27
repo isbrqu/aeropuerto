630bab53833f33ef128bf88e244f5f0bbc00f946
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-27 10:28:59 -0300

    hace uso de llenador para el ArbolAVL

--- src/conjuntistas/arbol/avl/test/TestArbolAVL.java
+++ src/conjuntistas/arbol/avl/test/TestArbolAVL.java
@@ -1,11 +1,24 @@
 package conjuntistas.arbol.avl.test;
 
 import conjuntistas.arbol.avl.ArbolAVL;
+import conjuntistas.arbol.manipulador.Llenador;
+import svgtree.Tree;
 
 public class TestArbolAVL {
 
   public static void main(String[] args) {
-    ArbolAVL a;
+    try {
+      ArbolAVL a = new ArbolAVL();
+      Llenador l = new Llenador(a);
+      int[] num = {2, 3, 4, 1, 5, 6, 19, 19};
+      l.llenar(num);
+      System.out.println(a);
+      Tree b = new Tree(a);
+      b.drawTree();
+      b.save("out/test.svg");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 } 

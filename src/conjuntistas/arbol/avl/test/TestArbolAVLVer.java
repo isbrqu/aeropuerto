5682ae11622d41dfc52d720c3157dbd300449ed2
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-07 18:30:05 -0300

    implementa la base del interprete de binario

--- src/conjuntistas/arbol/avl/test/TestArbolAVLVer.java
+++ src/conjuntistas/arbol/avl/test/TestArbolAVLVer.java
@@ -1,35 +1,20 @@
 package conjuntistas.arbol.avl.test;
 
 import conjuntistas.arbol.avl.ArbolAVL;
+import conjuntistas.arbol.avl.test.Creator;
 import svgtree.Html;
 
 public class TestArbolAVLVer {
 
   public static void main(String[] args) {
     try {
-      ArbolAVL arbol = new ArbolAVL();
-      String filename = "out/arbol.html";
-      arbol.insertar(1);
-      arbol.insertar(2);
-      arbol.insertar(3);
-      Html html = new Html();
-      html.draw(arbol, "Test Árbol");
-      arbol.insertar(5);
-      arbol.insertar(4);
-      arbol.insertar(-1);
-      arbol.insertar(-3);
-      html.draw(arbol, "Agrega -3");
-      arbol.insertar(-5);
-      arbol.insertar(7);
-      arbol.insertar(8);
-      arbol.insertar(10);
-      html.draw(arbol, "Agrega el 10");
-      arbol.insertar(11);
-      arbol.insertar(21);
-      html.draw(arbol, "Agrega el 5");
-      html.save(filename);
-      startTree(filename);
-      System.out.println(arbol);
+      System.out.println("hola");
+      // ArbolAVL arbol = new ArbolAVL();
+      // String filename = "out/arbol.html";
+      // Html html = new Html();
+      // html.save(filename);
+      // startTree(filename);
+      // System.out.println(arbol);
     } catch (Exception e) {
       e.printStackTrace();
     }

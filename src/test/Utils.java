b87692442539ec4356a78e8c6671c31f41d9d57f
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-08 10:48:15 -0300

    reindenta utils

--- src/test/Utils.java
+++ src/test/Utils.java
@@ -7,41 +7,41 @@ import lineales.dinamicas.Pila;
 
 public class Utils {
 
-    public static void llenar(Cola cola, String texto) {
-        cola.vaciar();
-        for (int i = 0; i < texto.length(); i++)
-            cola.poner(texto.charAt(i));
-    }
+  public static void llenar(Cola cola, String texto) {
+    cola.vaciar();
+    for (int i = 0; i < texto.length(); i++)
+      cola.poner(texto.charAt(i));
+  }
 
-    public static void llenar(Cola cola, int[] arr) {
-        cola.vaciar();
-        for (int i = 0; i < arr.length; i++)
-            cola.poner(arr[i]);
-    }
+  public static void llenar(Cola cola, int[] arr) {
+    cola.vaciar();
+    for (int i = 0; i < arr.length; i++)
+      cola.poner(arr[i]);
+  }
 
-    public static void llenar(Pila pila, String texto) {
-        pila.vaciar();
-        for (int i = 0; i < texto.length(); i++)
-            pila.apilar(texto.charAt(i));
-    }
+  public static void llenar(Pila pila, String texto) {
+    pila.vaciar();
+    for (int i = 0; i < texto.length(); i++)
+      pila.apilar(texto.charAt(i));
+  }
 
-    public static void llenar(Pila pila, int[] arr) {
-        pila.vaciar();
-        for (int i = 0; i < arr.length; i++)
-            pila.apilar(arr[i]);
-    }
+  public static void llenar(Pila pila, int[] arr) {
+    pila.vaciar();
+    for (int i = 0; i < arr.length; i++)
+      pila.apilar(arr[i]);
+  }
 
-    public static void llenar(Lista lista, String texto) {
-        lista.vaciar();
-        for (int i = 0; i < texto.length(); i++)
-            lista.insertar(texto.charAt(i), i + 1);
-    }
+  public static void llenar(Lista lista, String texto) {
+    lista.vaciar();
+    for (int i = 0; i < texto.length(); i++)
+      lista.insertar(texto.charAt(i), i + 1);
+  }
 
-    public static void llenar(Lista lista, int[] arr) {
-        lista.vaciar();
-        for (int i = 0; i < arr.length; i++)
-            lista.insertar(arr[i], i + 1);
-    }
+  public static void llenar(Lista lista, int[] arr) {
+    lista.vaciar();
+    for (int i = 0; i < arr.length; i++)
+      lista.insertar(arr[i], i + 1);
+  }
 
   public static Object[] variations(String[] alphabet, int length) {
     LinkedList<Integer> contador = new LinkedList<Integer>();

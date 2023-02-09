548f8e33c2d728801fb218eb727f660145efd0de
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-09 08:23:19 -0300

    interpreta los paréntesis incorrectamente

--- src/conjuntistas/arbol/avl/test/Creator.java
+++ src/conjuntistas/arbol/avl/test/Creator.java
@@ -2,12 +2,13 @@ package conjuntistas.arbol.avl.test;
 
 import conjuntistas.arbol.avl.NodoAVL;
 import lineales.dinamicas.Pila;
+import java.util.Arrays;
 
 public class Creator {
 
   public static void main(String[] args) {
     // sequence("10010");
-    // sysou
+    // System.out.println(Arrays.toString("hola".split("")));
   }
 
   public static NodoAVL base(String c) {
@@ -32,30 +33,48 @@ public class Creator {
     return root;
   }
 
-  // public static NodoAVL root(String pattern) {
-  //   String binaries = "01";
-  //   String bracket = "()";
-  //   String c;
-  //   boolean flag = true;
-  //   NodoAVL root = null;
-  //   NodoAVL child = null;
-  //   Pila tokens = new Pila();
-  //   String token;
-  //   String sequence;
-  //   for (int i = 0; i < pattern.length(); i++) {
-  //     c = Character.toString(pattern.charAt(i));
-  //     token = (String) tokens.obtenerTope();
-  //     if ("01(".contains(token)) {
-  //       tokens.apilar(token);
-  //     } else if (token.equals(")")) {
-  //       while (tokens.obtenerTope().equals("(")) {
-  //         sequence += (String) tokens.obtenerTope();
-  //         tokens.desapilar();
-  //       }
-  //       tokens.desapilar();
-  //     }
-  //   }
-  //   return root;
-  // }
+  public static NodoAVL root(String[] pattern) {
+    String binaries = "01";
+    String bracket = "()";
+    String c;
+    boolean flag = true;
+    NodoAVL root = null;
+    NodoAVL child = null;
+    Pila tokens = new Pila();
+    String sequence = "";
+    String token = null;
+    for (int i = 0; i < pattern.length; i++) {
+      c = pattern[i];
+      if (token == null || "01(".contains(token)) {
+        tokens.apilar(c);
+      } else if (token.equals(")")) {
+        while (token != null || !token.equals("(")) {
+          sequence += (String) tokens.obtenerTope();
+          tokens.desapilar();
+          token = (String) tokens.obtenerTope();
+        }
+        if (root == null) {
+          root = sequence(sequence.split(""));
+        } else {
+          child = root;
+          root = new NodoAVL(null, child, sequence(sequence.split("")));
+        }
+        tokens.desapilar();
+      }
+      token = (String) tokens.obtenerTope();
+    }
+    while (tokens.obtenerTope() != null) {
+      sequence += (String) tokens.obtenerTope();
+      tokens.desapilar();
+    }
+    if (root == null) {
+      root = sequence(sequence.split(""));
+    } else {
+      child = root;
+      root = new NodoAVL(null, child, sequence(sequence.split("")));
+    }
+    tokens.desapilar();
+    return root;
+  }
 
 }
